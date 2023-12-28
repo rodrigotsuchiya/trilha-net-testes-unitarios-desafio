cf242bf2c74883b2bb50e2454ffb87ad4c286c29
rodrigotsuchiya <rodri.tsuchiya@gmail.com>
2023-12-28 19:09:37 -0300

    fact

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -54,6 +54,7 @@ public class ValidacoesStringTests
     }
 
     //TODO: Corrigir a anotação [Fact]
+    [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
         //TODO: Corrigir a variável "textoProcurado" seção Arrange
